8627f3d634a3ccad7908578f3b8e9c3cc4ad97d3
tobit bushenyula <mrbushenyula@gmail.com>
2023-03-15 04:46:10 -0700

    Update Queue_and_stack.py
    here we are now implementing the queue and we are using a for loop to append elements inside that queue. further explanation are in the comment of the program we are working on

--- Queue_and_stack.py
+++ Queue_and_stack.py
@@ -2,13 +2,27 @@
 #STACK: its principle is LIFO (LAST IN, LAST OUT)
 
 from collections import deque
+#this "collections" is a library used for datastructure, we are going to use one of its functions or module called "deque" and this module or function will help us implement the queues and the stacks
+
 
 linked=deque()
 linked.append(56) #first appended
-linked.append(45.3) #second
-linked.append(20) #third
-linked.popleft() #this function is used to remove the last element addedd in a list (hee it is 20)
-print(linked)
+linked.append(45.3) #second appended
+linked.append(20) #third appended (at the end of the list coz, remember, the append function adds at the end of list)
+linked.pop() #this function is used to remove the last element added in a list (hee it is 20)
+linked.popleft() #this will remove the last element but at the left side (beginnig of the list)
+print(linked) #here we print that object after added some elements in it
+
+
+#!!!! LET US IMPLEMENT THE QUEUE NOW:
+
+queue=deque() #here we are now creating a queue. and for that we first store the deque function in the variable we called queue.
+for i in range(0,10): #then here we create this loop so that it will print the index from 0 to 9 [10 is not printed coz in the range function, the last element of the range is excluded]
+   queue.append(i) #here we will add the index number to our list in the queue everytime we append, until we reaches the last index which is 9
+   print(queue) #then here, since this print function is in the loop, everytime we apend, a queue list will be printed with the value appended added in, so we are going to jave 10 lists created where each following list will have one element more than the previous one.
+   
+
+
 
 
 
