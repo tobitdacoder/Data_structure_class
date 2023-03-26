c1aadc5b5c1d2b3da63cf8d2db7bfc07ee336cd3
tobit bushenyula <mrbushenyula@gmail.com>
2023-03-26 15:50:01 -0700

    Update stacks_in_python.py
    here is a little explanation on where we are going to use stacks in the real world (they are implemented in the backend using python and jango and flask as i know from now)

--- stacks_in_python.py
+++ stacks_in_python.py
@@ -3,3 +3,5 @@
 # using a list as a stack 
 # using deque as a stack
 #exercices
+
+'''first, stacks are usualy used in places like websites where, when we are on a website and we keep opening pages of that website, by clicking on the arrow (top-left) to come back to the previous page, there we are using stacks (Last In First Out - LIFO)... those links are in a stack where the last link we click on is the first to be closed once we click on that arrow to go back to the previous page (link)'''
