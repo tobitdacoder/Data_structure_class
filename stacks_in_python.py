8dfba5c2a550f72e4bc91dffbab476ef28cd2e4d
tobit bushenyula <mrbushenyula@gmail.com>
2023-04-04 19:29:32 -0700

    Update stacks_in_python.py
    Now, we are back with the introduction to the Stack data structure with more explanation before we dive into the explanation

--- stacks_in_python.py
+++ stacks_in_python.py
@@ -4,6 +4,10 @@
 # using deque as a stack
 #exercices
 
-'''first, stacks are usualy used in places like websites where, when we are on a website and we keep opening pages of that website, by clicking on the arrow (top-left) to come back to the previous page, there we are using stacks (Last In First Out - LIFO)... those links are in a stack where the last link we click on is the first to be closed once we click on that arrow to go back to the previous page (link)'''
+'''first, stacks are usualy used in places like websites where, when we are on a website and we keep opening pages of that website, by clicking on the arrow (top-left) to come back to the previous page, there we are using stacks data structure (Last In First Out - LIFO)... those links are in a stack where the last link we click on is the first to be closed once we click on that arrow to go back to the previous page (link)'''
 
 '''we are going to talk about queues just after this topic so stay tuned guyz 😊😁'''
+
+# first we have to know that push/pop are constant time operations O(1) 
+
+#when u are using the "undo" CTRL+Z functionality in any editor, u are using the stack to track down last set of operations
