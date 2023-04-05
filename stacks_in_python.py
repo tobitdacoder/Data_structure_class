f46389b53c7357dbb62b5caa502c83a3c19a3360
tobit bushenyula <mrbushenyula@gmail.com>
2023-04-05 10:38:19 -0700

    Update stacks_in_python.py
    here is the new class we created and this class is implementing the idea of stack. we created many functions inside which can be used either to append, to pop or even to return the length of our list

--- stacks_in_python.py
+++ stacks_in_python.py
@@ -46,5 +46,19 @@ print(stack)
 
 #now lets create a simple class implemnting the deque module:
 
+class stack:
+   def __init__(self):
+      self.container=deque()
+   def push(self,val):
+      return self.container.append(val)
+   def pop(self):
+      return self.container.pop()
+   def peek(self):
+      return self.container[-1]
+   def is_empty(self):
+      return len(self.container)==0
+   def size(self):
+      return len(self.container)
+
 
 
